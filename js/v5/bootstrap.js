d98834336e9bd955427c57054876be7a72d5f221
qoo109 <9s5nmzrjvj@privaterelay.appleid.com>
2026-07-16 17:07:21 +0800

    Wire V5.2 trends and router after render

--- js/v5/bootstrap.js
+++ js/v5/bootstrap.js
@@ -11,7 +11,10 @@
   function afterRender() {
     v5.modules.dashboard.afterRender();
     v5.modules.performanceDashboard?.afterRender();
+    v5.modules.performanceTrends?.afterRender();
     v5.modules.researchTimeline?.afterRender();
+    v5.modules.marketTrends?.afterRender();
+    v5.modules.router?.init();
     document.documentElement.dataset.uiPreparing = "false";
     document.documentElement.dataset.uiReady = "true";
   }

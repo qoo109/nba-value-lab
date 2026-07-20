ef4c30bb721bb8a93125b71c8d06ad9e7366d8d0
qoo109 <9s5nmzrjvj@privaterelay.appleid.com>
2026-07-20 14:37:12 +0800

    Refresh market overview UI cache

--- js/v4-init.js
+++ js/v4-init.js
@@ -84,7 +84,7 @@ function loadV49LockStatus() {
 
 function loadCurrentResearchStatus() {
   return loadScriptOnce(
-    "./js/current-research-status.js?v=20260719h",
+    "./js/current-research-status.js?v=20260720i",
     "data-current-research-status",
     () => document.documentElement.dataset.currentResearchStatus === "applied",
   );
